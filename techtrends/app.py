61fe30e523cb34522b225567572fd7a866f50c49
Chibundu <Chibunduogbonnia@gmail.com>
2022-10-11 19:55:34 +0200

    modifications on files

--- techtrends/app.py
+++ techtrends/app.py
@@ -92,5 +92,4 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0', port='3111')
